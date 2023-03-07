45d8c5be748c9d35d7b90b5fad32fcf7999e663a
Yussasif <99269469+Yussasif@users.noreply.github.com>
2023-03-06 23:40:51 -0800

    added functionalities

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
+let paymentPage = document.querySelector('#payment')
+let shopPage = document.querySelector('.shop-page')
+let groupsPage = document.querySelector('.groups-page')
+let cardsPage = document.querySelector('.cards-page')
 
-// if($(window).width() >= 1200){
-//     (function($) {
-//         var element = $('.to_move_content'),
-//             originalY = element.offset().top;
 
-//         // Space between element and top of screen (when scrolling)
-//         var topMargin = 10;
+let paymentBtn = document.querySelector('.payment-btn');
+let shopBtn = document.querySelector('.shop-btn');
+let groupsBtn = document.querySelector('.groups-btn');
+let cardsBtn = document.querySelector('.cards-btn');
 
-//         // Should probably be set in CSS; but here just for emphasis
-//         element.css('position', 'relative');
 
-//         $(window).on('scroll', function(event) {
-//             var scrollTop = $(window).scrollTop();
+function showPayment(){
+    paymentPage.classList.remove('hidden')
+    shopPage.classList.add('hidden')
+    groupsPage.classList.add('hidden')
+    cardsPage.classList.add('hidden')
+}
 
-//             element.stop(false, false).animate({
-//                 top: scrollTop < originalY
-//                     ? 0
-//                     : scrollTop - originalY + topMargin
-//             }, 0);
-//         });
-//     })(jQuery);
-// }
+function showShop(){
+    paymentPage.classList.add('hidden')
+    shopPage.classList.remove('hidden')
+    groupsPage.classList.add('hidden')
+    cardsPage.classList.add('hidden')
+}
 
-$(document).scroll(function() {
-    var y = $(document).scrollTop(), //get page y value 
-        header = $("#myarea"); // your div id
-    if(y >= 400)  {
-        header.css({position: "fixed", "top" : "0", "left" : "0"});
-    } else {
-        header.css("position", "static");
-    }
-});
+function showGroup(){
+    paymentPage.classList.add('hidden')
+    shopPage.classList.add('hidden')
+    groupsPage.classList.remove('hidden')
+    cardsPage.classList.add('hidden')
+}
+
+function showCard(){
+    paymentPage.classList.add('hidden')
+    shopPage.classList.add('hidden')
+    groupsPage.classList.add('hidden')
+    cardsPage.classList.remove('hidden')
+}
+
+
+
+paymentBtn.addEventListener('click', showPayment)
+shopBtn.addEventListener('click', showShop)
+groupsBtn.addEventListener('click', showGroup)
+cardsBtn.addEventListener('click', showCard)
+
